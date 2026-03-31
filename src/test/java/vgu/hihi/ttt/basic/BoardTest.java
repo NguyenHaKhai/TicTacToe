dabf9d0aecae24dabaaeaa486d0ba65a2304b80b
Khainh2005 <10423053@student.vgu.edu.vn>
2026-03-31 11:27:19 +0700

    simple tests for isFull, isCellEmpty

--- src/test/java/vgu/hihi/ttt/basic/BoardTest.java
+++ src/test/java/vgu/hihi/ttt/basic/BoardTest.java
@@ -1,6 +1,7 @@
 package vgu.hihi.ttt.basic;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -9,12 +10,32 @@ import org.junit.jupiter.api.Test;
 class BoardTest{
     //private Board testBoard = new Board(4,5);
 
-    @Test
+    @Test 
     void isFullTrue() {
+        Board testBoard = new Board(3, 3, new int[]{1,2,2,1,2,2,1,2,2});
+
+        assertTrue(testBoard.isFull());
+    }
+
+    @Test
+    void isFullFalse() {
         Board testBoard = new Board(3, 3, new int[]{1,2,2,1,2,0,1,2,2});
 
         assertFalse(testBoard.isFull());
     }
 
+    @Test
+    void cellEmptyTrue() {
+        Board testBoard = new Board(3, 3, new int[]{1,2,2,1,2,0,1,2,2});
+
+        assertTrue(testBoard.isCellEmpty(5));
+    }
+
+    @Test
+    void cellEmptyFalse() {
+        Board testBoard = new Board(3, 3, new int[]{1,2,2,1,2,0,1,2,2});
+
+        assertFalse(testBoard.isCellEmpty(0));
+    }
     
 }
