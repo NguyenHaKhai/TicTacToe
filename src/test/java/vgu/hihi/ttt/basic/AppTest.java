251bcca985dc52137b65775395339aa488462e80
NguyenHaKhai <nguyenhakhai2005@gmail.com>
2026-04-19 15:24:07 +0700

    write more tests

--- src/test/java/vgu/hihi/ttt/basic/AppTest.java
+++ src/test/java/vgu/hihi/ttt/basic/AppTest.java
@@ -22,8 +22,11 @@ class AppTest {
     private PipedOutputStream outputStream;
     private BufferedReader scanner;
 
+    private final java.io.InputStream originalIn = System.in;
+    private PipedOutputStream testOutToGameIn;
+
     @BeforeEach
-   void setUp() { 
+   void setUp() throws IOException { 
        
         outputStream = new PipedOutputStream();
         try {
@@ -33,10 +36,26 @@ class AppTest {
             // Logger.getLogger(AbstractPlayer.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-       System.setOut(new PrintStream(outputStream)); }
+       System.setOut(new PrintStream(outputStream)); 
+        
+       // set up system in to stimulate human player input
+        testOutToGameIn = new PipedOutputStream();
+        PipedInputStream gameReadStream = new PipedInputStream(testOutToGameIn);
+        System.setIn(gameReadStream);
+    }
 
     @AfterEach
-    void tearDown() { System.setOut(originalOut); }
+    void tearDown() throws IOException { 
+        System.setOut(originalOut); 
+        System.setIn(originalIn);
+        outputStream.close();
+        testOutToGameIn.close();
+    }
+
+    private void simulateTyping(String text) throws IOException {
+        testOutToGameIn.write((text + System.lineSeparator()).getBytes());
+        testOutToGameIn.flush();
+    }
 
     /**
      * Rigorous Test :-)
@@ -82,5 +101,76 @@ class AppTest {
     void startWQuotedArg() throws IOException{
         App.main(new String[]{"'1'"});
         assertEquals("Please, input a valid option [1-2]", scanner.readLine());
+    }
+
+    // Startup Message and Order
+    @Test
+    void startMessOrd() throws IOException{
+        App.main(new String[]{"1"});
+        assertEquals("Hello!", scanner.readLine());
+        assertEquals("| 0 | 0 | 0 |", scanner.readLine());
+        assertEquals("| 0 | 0 | 0 |", scanner.readLine());
+        assertEquals("| 0 | 0 | 0 |", scanner.readLine());
+        assertEquals("Player#1's turn", scanner.readLine());
+        
+        simulateTyping("q");
+    }
+
+    // Human Non-Integer Input
+    // Typing 3 next after x meaning that the program waits for output
+    // also, since the rep is changed: | 0 | 0 | 1 | -> mapping is correct 
+    @Test
+    void humanNonInt() throws IOException{
+        App.main(new String[]{"1"});
+        for(int i = 0; i < 5; i++) scanner.readLine();
+        simulateTyping("x");
+        simulateTyping("3");
+        
+        assertEquals("Please, input a valid number [1-9]", scanner.readLine());
+        
+        assertEquals("Player#1's turn", scanner.readLine());
+
+        assertEquals("| 0 | 0 | 1 |", scanner.readLine()); // game continues
+
+        simulateTyping("q");
+    }
+
+    // Human Out-of-Range Integer Input
+    @Test
+    void humanOutRangeIn() throws IOException{
+        App.main(new String[]{"1"});
+        for(int i = 0; i < 5; i++) scanner.readLine();
+        simulateTyping("0");
+        simulateTyping("10");
+        simulateTyping("-5");
+        
+        assertEquals("Please, input a valid number [1-9]", scanner.readLine());
+        assertEquals("Player#1's turn", scanner.readLine());
+
+        assertEquals("Please, input a valid number [1-9]", scanner.readLine());
+        assertEquals("Player#1's turn", scanner.readLine());
+
+        assertEquals("Please, input a valid number [1-9]", scanner.readLine());
+        assertEquals("Player#1's turn", scanner.readLine());
+
+        simulateTyping("q");
+    }
+
+    // Human Occupied Cell Input
+    @Test
+    void humanOccupiedIn() throws IOException{
+        App.main(new String[]{"2"});
+        simulateTyping("1");
+
+        // skip hello!, ini board, ... and computer turn, Player#1's turn
+        for(int i = 0; i < 10; i++) scanner.readLine();
+
+        simulateTyping("1");
+
+        assertEquals("The cell is occupied!", scanner.readLine());
+        assertEquals("Player#1's turn", scanner.readLine());
+
+        simulateTyping("q");
+
     }
 }
