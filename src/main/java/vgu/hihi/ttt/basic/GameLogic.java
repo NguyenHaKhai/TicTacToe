7f1a0fbd431b87dd6594d8a7c0622eddf6c13f57
NguyenHaKhai <nguyenhakhai2005@gmail.com>
2026-06-13 13:50:13 +0700

    add GameLogic to refactor

---
+++ src/main/java/vgu/hihi/ttt/basic/GameLogic.java
@@ -0,0 +1,20 @@
+package vgu.hihi.ttt.basic;
+
+public class GameLogic {
+    public static GameState applyMove(Board board, Player player, int move) {
+        switch (move) {
+            case -3:
+                return GameState.END;
+            case -2:
+                return GameState.OCCUPIED;
+            case -1:
+                return GameState.INVALID;
+            default:
+                board.setCell(move, player.getId());
+                // check for winner or draw after making the move
+                if (board.checkWinner3() == player.getId()) return GameState.WIN;
+                if (board.isFull()) return GameState.DRAW;
+                return GameState.CONT;
+        }
+    }
+}
